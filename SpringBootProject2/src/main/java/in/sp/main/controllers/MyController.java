ccf13b782903eee312252a6ad4030a43abcd6254
2370823 <2370823@LTIN509410.cts.com>
2024-11-29 10:53:50 +0530

    commit

--- SpringBootProject2/src/main/java/in/sp/main/controllers/MyController.java
+++ SpringBootProject2/src/main/java/in/sp/main/controllers/MyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-// Instead of giving @Controller and @Response body, we can give @RestController annotation
+// Instead of giving @Controller and @Response body, we can give @RestController annotation, I did some change
 public class MyController {
 	@GetMapping("/")
 	@ResponseBody
