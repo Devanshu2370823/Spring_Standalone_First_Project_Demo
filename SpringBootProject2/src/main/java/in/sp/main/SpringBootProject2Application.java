e33c8b33175d0de02ba174b4ee9d0d7080c88d31
2370823 <2370823@LTIN509410.cts.com>
2024-12-04 11:43:16 +0530

    Commit added

--- SpringBootProject2/src/main/java/in/sp/main/SpringBootProject2Application.java
+++ SpringBootProject2/src/main/java/in/sp/main/SpringBootProject2Application.java
@@ -3,6 +3,7 @@ package in.sp.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Hello this is main class
 @SpringBootApplication
 public class SpringBootProject2Application {
 
