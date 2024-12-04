d851101486b2579caa9bbec4a8c24f58d86a2558
2370823 <2370823@LTIN509410.cts.com>
2024-12-04 11:50:25 +0530

    Committed hi

--- SpringBootProject1/src/main/java/in/sp/main/SpringBootProject1Application.java
+++ SpringBootProject1/src/main/java/in/sp/main/SpringBootProject1Application.java
@@ -3,6 +3,7 @@ package in.sp.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Hello this is another commit
 @SpringBootApplication
 public class SpringBootProject1Application {
 
